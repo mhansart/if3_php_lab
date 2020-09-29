c7351f567609e8b1cf7fb62131f1131cd4529581
Marine Hansart <hansart.marine@gmail.com>
2020-09-29 10:12:13 +0200

    change title

--- view/html/head.php
+++ view/html/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
-    <title>Le site des web</title>
+    <title>Skywalkers Ultimate</title>
 </head>
 <body>
     
