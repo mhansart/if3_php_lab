d703279382a99daf532d44bace8aee8bc0f704de
Marine Hansart <hansart.marine@gmail.com>
2020-09-29 13:23:18 +0200

    style accueil

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+var h = window.innerHeight;
+var style = document.createElement("style");
+document.head.appendChild(style);
+style.sheet.insertRule("body {height: " + h + "px}");
--- view/page/accueil.php
+++ view/page/accueil.php
@@ -1,8 +1,12 @@
 
 
 <div class="accueil">
-    <div class="logo_big"><img src="public/img/logo.png" alt="logo"></div>
-    <?php include("view/menu/menu.php");  ?>
+    <div class="containerAccueil">
+        <div class="logo_big"><img src="public/img/logo.png" alt="logo"></div>
+        <p class="title title--big">Club d'ultimate frisbee</p>
+        <p class="title title--little">• <span class="point"> •</span> à Braine l'Alleud et à la Hulpe <span class="point"> •</span> •</p>
+        <?php include("view/menu/menu.php");  ?>
+    </div>
 </div>
 <script src="public/js/script.js"></script>
 <?php include("view/html/footer.php"); ?>
