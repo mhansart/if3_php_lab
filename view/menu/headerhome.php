8ef38b6eebddacc5e18fe116b41be3f3550bd2da
Hansart Marine <hansart.marine@gmail.com4>
2020-09-30 22:09:29 +0200

    change footer

---
+++ view/menu/headerhome.php
@@ -0,0 +1,5 @@
+<header class="headerHome">
+    <div class="containerHeaderHome">
+            <?php include("view/menu/menuconnexion.php");  ?>
+        </div>
+</header>
--- view/page/accueil.php
+++ view/page/accueil.php
@@ -1,15 +1,12 @@
 
-<header class="headerAccueil">
-    <div class="containerHeaderAccueil">
-        <?php include("view/menu/menuconnexion.php");  ?>
-    </div>
-</header>
-<div class="accueil">
-    <div class="containerAccueil">
-        <div class="logo_big"><img src="public/img/logo.png" alt="logo"></div>
-        <h1 class="title title--big">Club d'ultimate frisbee</h1>
-        <h2 class="title title--little"><span class="point">•</span> à Braine l'Alleud et à la Hulpe <span class="point">•</span></h2>
-        <?php include("view/menu/menuaccueil.php");  ?>
+    <?php include("view/menu/headerhome.php");  ?>
+    <div class="accueil">
+        <div class="containerAccueil">
+            <div class="logo_big"><img src="public/img/logo.png" alt="logo"></div>
+            <h1 class="title title--big">Club d'ultimate frisbee</h1>
+            <h2 class="title title--little"><span class="point">•</span> à Braine l'Alleud et à la Hulpe <span class="point">•</span></h2>
+            <?php include("view/menu/menuaccueil.php");  ?>
+        </div>
     </div>
 </div>
 <script src="public/js/script.js"></script>
