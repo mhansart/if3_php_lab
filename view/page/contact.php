199c82b32462da8cd654d5539d586e87f2b4cf64
Marine Hansart <hansart.marine@gmail.com>
2020-10-01 16:36:00 +0200

    contact page

--- view/page/contact.php
+++ view/page/contact.php
@@ -1,13 +1,63 @@
 <?php include("view/menu/menu.php");  ?>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<div class="containerWidth">
+    <div class="p-i-flex">
+        <div class="imgContact"><img src="public/img/contact.jpg" alt="contact"></div>
+    <form action="#" method="POST">
+        <h2>Contactez nous</h2>
+        <label for="nom">Nom</label>
+        <input type="text" name="nom" id="nom">
+        <label for="prenom">Prénom</label>
+        <input type="text" name="prenom" id="prenom">
+        <label for="email">E-mail</label>
+        <input type="text" name="email" id="email" required>
+        <label for="message">Message</label>
+        <textarea type="text" name="message" id="message" required></textarea>
+        <button type="submit">Envoyer</button>
+    </form>
+    <?php
+        require("phpmailer/class.phpmailer.php");
+        
+        $mail = new PHPMailer();
+        
+        $mail->IsSMTP(); //On utilise le mode SMTP pour la lib mailer
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true; //authentification requis
+        $mail->Username = "hansart.marine@gmail.com"; // SMTP username
+        $mail->Password = "heff2sma"; // SMTP password
+        
+        $mail->From = "monsite@mondomaine.fr";
+        $mail->FromName = "nom";
+        $mail->AddAddress("xxxx@xxxx.com");
+        
+        
+        $mail->Subject = "Sujet";
+        $mail->Body = "Le message";
+        
+        if(!$mail->Send())
+        {
+        echo "Message could not be sent. <p>";
+        echo "Mailer Error: " . $mail->ErrorInfo;
+        exit;
+        }
+        
+        echo "Message has been sent";
+    ?>
+    <?php
+    if (isset($_POST['message'])) {
+        $position_arobase = strpos($_POST['email'], '@');
+        if ($position_arobase === false)
+            echo '<p>Votre email doit comporter un arobase.</p>';
+        else {
+            $retour = mail('hansart.marine@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: ' . $_POST['email']);
+            if($retour)
+                echo '<p>Votre message a été envoyé.</p>';
+            else
+                echo '<p>Erreur.</p>';
+        }
+    }
+    ?>
+    </div>
+</div>
 <?php include("view/menu/menufooter.php");  ?>
 <script src="public/js/script.js"></script>
 <?php include("view/html/footer.php"); ?>
--- view/page/lultimate.php
+++ view/page/lultimate.php
@@ -10,9 +10,27 @@
             <div class="btnBasis"><a href="#"> Découvrez les règles<i class="fas fa-caret-right"></i></a></div>
         </div>
         <div class="picture"><img src="public/img/ultimate.jpg" alt="ultimate"></div>
-            
         </div>
     </div>
+    <div class="spirit">
+            <div class="textContent">
+                <h3 class="bold">Spirit of the Game</h3>
+                <p>La particularité de l’Ultimate Frisbee est l’<span class="bold">absence d’arbitre</span>, même en compétition officielle. Chaque joueur sur le terrain est son propre arbitre, il appellera lui-même lorsqu’il estime qu’un faute a été commise. S’il y a un désaccord entre deux joueurs, après discussion, le disque reviendra une phase en arrière pour que le jeu puisse reprendre. Dès lors, il n’est plus possible de se réfugier derrière un mauvaise arbitrage, <span class="bold">seule la meilleure équipe gagnera.</span></p>
+                <p>À l’issue de chaque match, les équipes évaluent l’équipe adverse selon certains critères afin d’établir <span class="bold">un classement du « Spirit of the Game ».</span> Ce classement a autant de valeur que le classement général, et est un objectif à part entière pour chaque équipe.</p>
+                <p><span class="bold">Toutes ces valeurs font de l’Ultimate Frisbee un sport tel que le Sport (avec un grand « S ») voudrait qu’il soit.</span></p>
+            </div>
+        </div>
+        <div class="containerWidth">   
+            <div class="p-i-flex">
+                <div class="picture"><img src="public/img/valeurs.jpg" alt="ultimate"></div>
+                <div class="textContent"> 
+                    <h3 class="bold">Les valeurs</h3>
+                    <p><span class="bold">La vitesse, la capacité athlétique, la tactique, la technique,</span> tant de choses nécessaires pour pratiquer correctement ce sport. Voici une des raisons pour laquelle nos joueurs s'entraînent <span class="bold">une à trois fois par semaine. </p>
+                    <p><span class="bold">La sportivité</span> est la seconde raison, au moins aussi importante que la première.Pour pratiquer un sport, il s’agit de bien <span class="bold">maîtriser les règles et savoir les appliquer correctement.</span> </p>
+                    <p class="lastP">Les maîtres-mots de la discipline sont <span class="bold">« Sportivité », « Respect » et « Fair-play ».</span></p>
+                </div>
+                </div>
+            </div>
 <?php include("view/menu/menufooter.php");  ?>
 <script src="public/js/script.js"></script>
 <?php include("view/html/footer.php"); ?>
